336e237a56ef66323c30384b97789d28096f4bde
sonykikuma <er.abhasoni@gmail.com>
2024-09-10 13:05:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const { initializeDatabase } = require("./db/db.connection");
+const { Books1 } = require("./models/books1.model");
+
+app.use(cors());
+app.use(express.json());
+
+initializeDatabase();
+
+app.get("/", (req, res) => {
+  res.send("Hello, Express!");
+});
+
+app.get("/books", async (req, res) => {
+  try {
+    const allbooks = await Books1.find();
+    res.json(allbooks);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.post("/books", async (req, res) => {
+  const { bookName, author, genre } = req.body;
+
+  try {
+    const bookData = new Books1({ bookName, author, genre });
+    await bookData.save();
+    res.status(201).json(bookData);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.delete("/books/:id", async (req, res) => {
+  const bookId = req.params.id;
+
+  try {
+    const deletedBook = await Books1.findByIdAndRemove(bookId);
+
+    if (!deletedBook) {
+      return res.status(404).json({ error: "Book not found" });
+    }
+
+    res.status(200).json({
+      message: "Book deleted successfully",
+      book: deletedBook,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ models/books1.model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const bookSchema1 = new mongoose.Schema({
+  bookName: String,
+  author: String,
+  genre: String,
+});
+
+const Books1 = mongoose.model("Books1", bookSchema1);
+
+module.exports = { Books1 };
