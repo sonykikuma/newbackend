5507916f996396167ede0df5592f043b63fca201
sonykikuma <er.abhasoni@gmail.com>
2024-09-13 15:58:43 +0530

    trying to fix deployment

--- models/books1.model.js
+++ models/books1.model.js
@@ -10,3 +10,5 @@ const Books1 = mongoose.model("Books1", bookSchema1);
 
 module.exports = { Books1 };
 // "src": "./index.js",
+//  "buildCommand": "next build",
+//      "methods": ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
